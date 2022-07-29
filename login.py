5e2ec80f6b87b083574bbb6601c382927ed94c78
LordAdamantine <raymond.fillingame@eagles.usm.edu>
2022-07-29 13:28:50 -0500

    Patch 0.3, file repairs

---
+++ login.py
@@ -0,0 +1,8 @@
+import os
+import logging
+from pymongo import MongoClient
+import phb_store
+
+def login():
+    logging.info("")
+    menu_option = input("")
--- phb_store.py
+++ phb_store.py
@@ -1,8 +1,8 @@
 import re
 import logging
-import mysql.connector
-import mysql_config as c
+from pymongo import MongoClient
 import os
+import login
 
 '''
 To Do:
@@ -23,27 +23,74 @@ Different collections for each to help differentiate.
 '''
 
 def main():
-    clear = lambda: os.system('cls')    #use clear() within code to clear console between certain screens.
 
+    # Pre initialize vital files
+    logging.basicConfig(filename = "store.log", level = logging.DEBUG, format = '%(asctime)s :: %(message)s')
+    clear = lambda: os.system('cls')    #use clear() within code to clear console between certain screens.
+    client = MongoClient()
+    db = client.get_database("Project1")
+
+    armor = db.armor
+    weapons = db.weapons
+    gear = db.gear
+    misc = db.misc
+    users = db.users
+    logging.info("Loading database records...")
+
+    # Welcome ascii art, I worked very hard on it
+    with open('welcome.txt') as f:
+        intro = f.read()
+    print(str(intro))
     
     while True: #Keep majority of code, probably including login, inside while True loop.
         # Welcome screen and login.
-        print("\t\tWelcome to the Sharpest Tool in the Shed!\n\t\t\tPlease log in or create account!")
+        input("\t\t\t\tWelcome to the Sharpest Tool in the Shed!\n\t\t\t\tPlease log in or create an account!")
+        
+        '''test_print(armor, weapons, gear, misc)'''
+
         # Probably separate module/s with login functionality/account creation/quit option.
-        # # account() module to login/create account.
-        clear()
+
+        #login()
+        # account() module to login/create account.
+        #clear()
+
         while True:
             # Modules depending on account class.
                 #User: purchase, history, reserve, etc.
                 #Admin: view all orders, view customer lists, modify accounts and inventory.
             # Close out all files and remember to commit all db changes on log out...
-            clear()
-            pass
-        # Loop back around to login
-        pass
-    # Include some kind of parting farewell thanking the customer.
-    pass
+            #clear()
+            break
 
+        # Loop back around to login until quit
+        break
+
+    # Include some kind of parting farewell thanking the customer upon breaking out and quitting.
+    with open("thanks.txt") as f:
+        outro = f.read()
+    print(str(outro))
+    return 0
+
+
+def test_print(armor, weapons, gear, misc):
+    armor_test = armor.find({}, {'name':1, '_id':0})
+    weapons_test = weapons.find({}, {'name':1, '_id':0})
+    gear_test = gear.find({}, {'name':1, '_id':0})
+    misc_test = misc.find({}, {'name':1, '_id':0})
+
+    print("\n" + "Armor Sets")
+    for elem in armor_test:
+        print(elem)
+    print("\n" + "Weapons")
+    for elem in weapons_test:
+        print(elem)
+    print("\n" + "Miscellaneous Gear")
+    for elem in gear_test:
+        print(elem)
+    print("\n" + "Magical Baubles")
+    for elem in misc_test:
+        print(elem)
+    print("\n")
 
 
 if __name__ == "__main__":
